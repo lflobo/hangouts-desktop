ff566572ce0b3c6b7ee291d28d45e731f5434945
Luís Lobo <ellobo@ipb.pt>
2016-07-07 10:06:46 +0100

    Initial Commit

---
+++ src/main/java/pt/lobo/tools/hangouts/Hangouts.java
@@ -0,0 +1,25 @@
+package pt.lobo.tools.hangouts;
+
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Hangouts extends JFrame {
+
+    public Hangouts() throws HeadlessException {
+        super("Hangouts");
+
+        Browser browser = new Browser();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(new BrowserView(browser), BorderLayout.CENTER);
+        setSize(800, 600);
+        browser.loadURL("https://hangouts.google.com");
+    }
+
+    public static void main(String[] args) {
+        new Hangouts().setVisible(true);
+    }
+
+}
